0ea2c43fce63d0a506cf00a4735a6a48f12a67c3
Mark L. Baird <bairdml@ornl.gov>
2018-05-17 11:50:51 -0400

    Updating PETSC and SLEPC respectively to 3.6.4 and 3.6.3 respectively as part of the GCC-5.4.0 update 5341

--- petsc/3.3-p4/include/petsccusp.h
+++
@@ -1,8 +0,0 @@
-#if !defined(__PETSCCUSP_H)
-#define __PETSCCUSP_H
-/*
-    This should only be included in user code that uses CUSP directly and hence the file name ends with .cu
-*/
-#include "../src/vec/vec/impls/dvecimpl.h"
-#include "../src/vec/vec/impls/seq/seqcusp/cuspvecimpl.h"
-#endif
